1a18f2fad65af7a5c29153afad9091154dc70c50
FrosteeBoi <frosteeboiii@gmail.com>
2025-09-17 21:09:43 +0100

    initial commit

--- ui/dashboard.py
+++ ui/dashboard.py
@@ -5,7 +5,7 @@ from logic.user import User
 import re
 from datetime import datetime, date
 from db.db_handler import save_user_to_db
-import sqlite3
+from measurement import Measurement
 
 
 class Dashboard:
@@ -22,11 +22,11 @@ class Dashboard:
 
         # Label that welcomes user
         self.dash_label = tb.Label(self.dashframe, text=f"Hello {self.user.username}", font=("roboto", 18, "bold"))
-        self.dash_label.grid(row=0, column=0, pady=(20, 275), sticky="n")
+        self.dash_label.grid(row=0, column=0, pady=(20, 50), sticky="n")
 
         # Bottom frame for tabs
         self.tab_frame = tb.Frame(self.dashframe)
-        self.tab_frame.grid(row=1, column=0, sticky="n", pady=(275, 20), padx=5)
+        self.tab_frame.grid(row=4, column=0, sticky="n", pady=(420, 20), padx=5)
 
         # Creates buttons for the tabs
         self.measurements_button = tb.Button(self.tab_frame, text="Measurements", command=self.show_measurements)
@@ -35,6 +35,11 @@ class Dashboard:
         self.sleep_button = tb.Button(self.tab_frame, text="Sleep", command=self.show_sleep)
         self.steps_button = tb.Button(self.tab_frame, text="Steps", command=self.show_steps)
 
+        # Creates the visual displays
+        self.dash_steps = tb.Label(self.dashframe, text="Steps: 6767")
+        self.dash_cals = tb.Label(self.dashframe, text="Calories: 6767")
+        self.dash_sleep = tb.Label(self.dashframe, text="Sleep: 6767")
+
         # Grids the buttons
         self.measurements_button.grid(row=0, column=0, padx=5)
         self.food_button.grid(row=0, column=1, padx=5)
@@ -42,9 +47,14 @@ class Dashboard:
         self.sleep_button.grid(row=0, column=3, padx=5)
         self.steps_button.grid(row=0, column=4, padx=5)
 
+        # Grids the visual displays:
+        self.dash_steps.grid(row=1, column=0)
+        self.dash_cals.grid(row=2, column=0)
+        self.dash_sleep.grid(row=3, column=0)
+
     # Placeholder methods for each tab
     def show_measurements(self):
-        print("Show metrics")
+        Measurement(self.root, self.user)
 
     def show_food(self):
         print("Show food")
--- ui/measurement.py
+++ ui/measurement.py
@@ -1,4 +1,3 @@
-
 import ttkbootstrap as tb
 import random
 from tkinter import messagebox
@@ -7,7 +6,6 @@ import re
 from datetime import datetime, date
 from db.db_handler import save_user_to_db
 import sqlite3
-
 
 
 class Measurement:
@@ -23,24 +21,29 @@ class Measurement:
         self.root.grid_columnconfigure(0, weight=1)
 
         # Label that welcomes user
-        self.measure_label = tb.Label(self.measureframe, text=f"{self.user.username}'s Measurements", font=("roboto", 18, "bold"))
+        self.measure_label = tb.Label(self.measureframe, text=f"{self.user.username}'s Measurements",
+                                      font=("roboto", 18, "bold"))
         self.measure_label.grid(row=0, column=0, pady=(50, 50), padx=(100, 0), sticky="n")
 
-        self.height_label = tb.Label(self.measureframe, text="Height", font=("roboto", 18, "bold"))
-        self.height_label.grid(row=1, column=0, pady=(50, 50), sticky="w")
+        # Height, weight and bmi labels and entries
+        self.height_label = tb.Label(self.measureframe, text="Height: \n 174cm", font=("roboto", 18, "bold"))
+        self.height_label.grid(row=1, column=0, pady=(50, 0), sticky="w")
 
-        self.weight_label = tb.Label(self.measureframe, text="Weight", font=("roboto", 18, "bold"))
+        self.weight_label = tb.Label(self.measureframe, text="Weight: \n 69kg", font=("roboto", 18, "bold"))
         self.weight_label.grid(row=1, column=1, pady=(50, 50), sticky="n")
 
-        self.bmi_label = tb.Label(self.measureframe, text="Body Mass Index", font=("roboto", 18, "bold"))
-        self.bmi_label.grid(row=2, column=0, pady=(50, 50), sticky="n")
+        self.bmi_label = tb.Label(self.measureframe, text="Body Mass Index: \n 22.8", font=("roboto", 18, "bold"))
+        self.bmi_label.grid(row=3, column=0, pady=(50, 50), sticky="n")
 
+        self.height_entry = tb.Entry(self.measureframe)
+        self.height_entry.grid(row=2, column=0, sticky="w", pady=(0, 50))
 
+        self.weight_entry = tb.Entry(self.measureframe)
+        self.weight_entry.grid(row=2, column=1, sticky="n", pady=(0, 50))
 
 
 
 if __name__ == "__main__":
-
     root = tb.Window(themename="darkly")
     test_user = User("TestUser", "1234567", "Male", "26/12/2007", "29/082025")
     app = Measurement(root, test_user)
